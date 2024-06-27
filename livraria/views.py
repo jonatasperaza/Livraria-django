938d3c465ceeadc442af3f06d5b189608f44a007
jonatasperaza <jonatassilvaperaza@gmail.com>
2024-06-27 15:41:33 -0300

    chore: Add djangorestframework to requirements and pyproject

--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from livraria.models import Categoria
+from livraria.serializers import CategoriaSerializer
+from livraria.models import Editora
+from livraria.serializers import EditoraSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
